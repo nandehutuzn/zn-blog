36b1a3aee1e9dc1052d4c4648ae2cfc34c3f2b84
zhang.ning <zhang.ning@mdb-fintech.com>
2020-10-16 10:56:12 +0800

    测试CI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Zn GitHub Pages"/>
+    <HelloWorld msg="Welcome to Znnnnn GitHub Pages"/>
   </div>
 </template>
 <script>
