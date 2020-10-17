6a72fc958794e6aa82f0e74e2e53f0d1f9f4cb16
zhang.ning <zhang.ning@mdb-fintech.com>
2020-10-17 10:14:53 +0800

    采用vuepress框架

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  title: '张宁-个人博客',
+  description: '前端、C#、算法、生活......',
+  markdown: {
+    lineNumbers: true
+  },
+  plugins: [
+    // 你可以多次使用这个插件
+    [
+      'vuepress-plugin-container',
+      {
+        type: 'right',
+        defaultTitle: '',
+      },
+    ],
+    [
+      'vuepress-plugin-container',
+      {
+        type: 'theorem',
+        before: info => `<div class="theorem"><p class="title">${info}</p>`,
+        after: '</div>',
+      },
+    ],
+
+    // 这是 VuePress 默认主题使用这个插件的方式
+    [
+      'vuepress-plugin-container',
+      {
+        type: 'tip',
+        defaultTitle: {
+          '/': 'TIP',
+          '/zh/': '提示',
+        },
+      },
+    ],
+  ],
+}
