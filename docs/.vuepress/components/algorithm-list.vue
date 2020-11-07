2c639166c8dcc89c66e93863f8bc51692c621ea9
zhang.ning <zhang.ning@mdb-fintech.com>
2020-11-07 15:29:26 +0800

    阿里算法题

--- docs/.vuepress/components/algorithm-list.vue
+++ docs/.vuepress/components/algorithm-list.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="list">
-    <a href="/algorithm/20201105">分别用JS Array 和 LinkedList 实现 LRU</a>
+    <a href="/algorithm/20201107">2020阿里前端的两个算法题</a>
+    <a href="/algorithm/20201105">分别用JS Array 和 LinkedList 实现 LRU（字节跳动）</a>
     <a href="/algorithm/20201104">几个二叉树相关的算法题</a>
     <a href="/algorithm/20201103">2020美团前端的几个算法题</a>
     <a href="https://mp.weixin.qq.com/s?__biz=MzIwNjc5ODA1Mw==&mid=2247484065&idx=1&sn=c9404891561742aa02a4f7bd7e0e25d6&chksm=971d6f2ba06ae63d4e029d99f72348f0b31a26b0b9312e6444a70cd4d09adb912ced9f416d65&token=181237259&lang=zh_CN#rd">跟着Google大佬重学算法（一）</a>
